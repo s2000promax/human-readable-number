edab4fd09f8698448ca7234ab8ac2e7a175b9de3
Stan <s2000ProMax@gmail.com>
2022-01-21 00:19:03 +0300

    init: creat function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function toReadable (number) {
-  
+  const dictionary = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  return dictionary[number];
 }
