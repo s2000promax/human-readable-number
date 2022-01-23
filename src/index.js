fd6b4063fa3c5dc9772c2bb7e9167314e4280980
Stan <s2000ProMax@gmail.com>
2022-01-23 16:48:55 +0300

    update: update code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 module.exports = function toReadable (number) {
-  const dictionary = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  return dictionary[number];
+   const dictionary0 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', ''];
+  const dictionary10 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const dictionary20 = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const dictionary100 = ['hundred'];
+
+  if (number < 10) return `${dictionary0[number]}`;
+  if (number >= 10 && number <= 19) return `${dictionary10[number - 10]}`
+  if (number >= 20 && number <= 99) return `${dictionary20[Number((number + '')[0])]} ${Number((number + '')[1]) !== 0 ? dictionary0[Number((number + '')[1])] : ''}`
+
+  if (number >= 100) {
+    const str12numb = Number((number + '')[1] + (number + '')[2]);
+    return `${
+    dictionary0[(number + '')[0]]
+  } ${
+    dictionary100[0]
+  } ${
+      str12numb > 0 && str12numb < 10 ? dictionary0[(number + '')[2]] 
+        : str12numb >= 10 && str12numb <= 19 ? dictionary10[str12numb - 10] 
+          : str12numb >= 20 && str12numb <= 99 ? dictionary20[(number + '')[1]] + " " + dictionary0[(number + '')[2] == 0 ? 10 : (number + '')[2]] 
+            : ''
+  } `
+}
+
 }
